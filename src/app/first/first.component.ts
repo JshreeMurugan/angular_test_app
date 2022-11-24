0c20d8073162fa9857078aee13085489cd4a676c
jeyashree.murugan <jeyashree.murugan@ameexusa.com>
2022-11-24 22:47:05 +0530

    Feat: rxjs Operators

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-first',
   templateUrl: './first.component.html',
   styleUrls: ['./first.component.scss']
 })
-export class FirstComponent implements OnInit {
+export class FirstComponent implements OnInit, OnChanges, DoCheck, AfterViewInit,
+AfterContentInit, AfterViewChecked, AfterContentChecked, AfterViewChecked, OnDestroy {
 
-  constructor() { }
+order = 1;
+@Input() footerText;
 
-  ngOnInit(): void {
-  }
+constructor() {
+console.log('I am from constructor()!! and my order::::' + this.order);
+this.order++;
+}
+
+ngOnChanges(changes: SimpleChanges) {
+console.log('I am from ngOnChanges()!! and my order::::' + this.order);
+this.order++;
+}
+
+ngOnInit() {
+console.log('I am from ngOnInit() and my order::::' + this.order);
+this.order++;
+}
+
+ngDoCheck() {
+console.log('I am from ngDoCheck() and my order::::' + this.order);
+this.order++;
+}
+
+ngAfterContentInit() {
+console.log('I am from ngAfterContentInit() and my order::::' + this.order);
+this.order++;
+}
+
+ngAfterContentChecked() {
+console.log('I am from ngAfterContentChecked() and my order::::' + this.order);
+this.order++;
+}
+
+ngAfterViewInit() {
+console.log('I am from ngAfterViewInit() and my order::::' + this.order);
+this.order++;
+}
+
+ngAfterViewChecked() {
+console.log('I am from ngAfterViewChecked() and my order::::' + this.order);
+this.order++;
+}
+
+ngOnDestroy() {
+console.log('I am from ngOnDestroy() and my order::::' + this.order);
+this.order++;
+}
 
 }
+
+
--- src/app/new/new.component.ts
+++ src/app/new/new.component.ts
@@ -1,8 +1,8 @@
 import { removeSummaryDuplicates } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { from, fromEvent, interval, Subject, Subscription, forkJoin, of } from 'rxjs';
-import { map, debounceTime, distinctUntilChanged, throttleTime, pluck, catchError, mergeMap } from 'rxjs/operators';
+import { from, fromEvent, interval, Subject, Subscription, forkJoin, of, combineLatest, timer, concat, empty, zip } from 'rxjs';
+import { map, debounceTime, distinctUntilChanged, throttleTime, pluck, catchError, mergeMap, delay, startWith, concatAll, tap, filter, concatMap, exhaustMap, switchMap, take, exhaustAll } from 'rxjs/operators';
 import { ajax } from 'rxjs/ajax';
 @Component({
   selector: 'app-new',
@@ -93,6 +93,15 @@ export class NewComponent implements OnInit {
     this.pluckOperator();
     this.catchOperator();
     this.mergeMapOperator();
+    this.combineLatestOperator();
+    this.concatOperator();
+    this.concatAllOperator();
+    this.zipOperator();
+    this.tapOperator();
+    this.conCatMapOperator();
+    this.exhaustMapOperator();
+    this.switchMapOperator();
+    this.exhaustAllOperator();
   }
 
   throttleTimeOperator() {
@@ -155,9 +164,9 @@ export class NewComponent implements OnInit {
 
   mergeMapOperator() {
     console.log('mergeMapOperator');
-    
+
     const letters = of('a', 'b', 'c');
-    const numbers = of (1,2,3)
+    const numbers = of(1, 2, 3)
     const result = letters.pipe(
       mergeMap(x => numbers.pipe(map(i => x + i))),
     );
@@ -178,6 +187,181 @@ export class NewComponent implements OnInit {
     // b1
     // c1
     // if setInterval continues to list a,b,c with respective ascending integers
+  }
+
+  combineLatestOperator() {
+    // // 1. Define multiple observables
+    // let color = of('Black', 'Red', 'Blue');
+    // let brand = of('Jaguar', 'Ford', 'BMW');
+    // let price = of(100, 200, 300);
+
+    // // 2. Call combineLatest operator, inject multiple observables in array
+    // const joinStream = combineLatest(color, brand, price);
+
+    // // 3. Subscibe combineLatest observable
+    // const subscribe = joinStream.subscribe(([color, brand, price]) => {
+    //   console.log(color + ' ' + brand + ' ' + 'costed me' + ' ' + price);
+    // });
+
+    // timerOne emits first value at 1s, then once every 4s
+    const timerOne$ = timer(1000, 4000);
+    // timerTwo emits first value at 2s, then once every 4s
+    const timerTwo$ = timer(2000, 4000);
+    // timerThree emits first value at 3s, then once every 4s
+    const timerThree$ = timer(3000, 4000);
+
+    // when one timer emits, emit the latest values from each timer as an array
+    combineLatest(timerOne$, timerTwo$, timerThree$).subscribe(
+      ([timerValOne, timerValTwo, timerValThree]) => {
+        /*
+          Example:
+        timerThree first tick: 'Timer One Latest: 0, Timer Two Latest: 0, Timer Three Latest: 0
+        timerOne second tick: 'Timer One Latest: 1, Timer Two Latest: 0, Timer Three Latest: 0
+        timerTwo second tick: 'Timer One Latest: 1, Timer Two Latest: 1, Timer Three Latest: 0
+      */
+        console.log(
+          `Timer One Latest: ${timerValOne},
+     Timer Two Latest: ${timerValTwo},
+     Timer Three Latest: ${timerValThree}`
+        );
+      }
+    );
+  }
+
+  concatOperator() {
+    console.log('concatOperator');
+    concat(
+      of(1, 2, 3),
+      // subscribed after first completes
+      of(4, 5, 6),
+      // subscribed after second completes
+      of(7, 8, 9)
+    ).subscribe(console.log);
+
+    const userMessage = document.getElementById('message');
+
+    const delayedMessage = (message, delayedTime = 1000) => {
+      return empty().pipe(startWith(message), delay(delayedTime));
+    }
+    if (userMessage != null) {
+      concat(delayedMessage('message'),
+        delayedMessage('1'),
+        delayedMessage('2'),
+        delayedMessage('3'),
+        delayedMessage('', 2000)
+      ).subscribe(message => userMessage.innerHTML = message);
+    }
+
+  }
+
+
+  concatAllOperator() {
+    console.log('concatAllOperator');
+    const source = interval(1000);
+    const example = source.pipe(
+      map(val => of(val + 10)), concatAll()
+    )
+
+    example.subscribe(val => { console.log('hi' + val) });
+  }
+
+  zipOperator() {
+    console.log('zipOperator');
+
+    let age$ = of(27, 25, 29);
+    let name$ = of('Foo', 'Bar', 'Beer');
+    let isDev$ = of(true, true, false);
+
+    zip(age$, name$, isDev$).pipe(
+      map(([age, name, isDev]) => ({ age, name, isDev })),
+    )
+      .subscribe(x => console.log(x));
+
+    // outputs
+    // { age: 27, name: 'Foo', isDev: true }
+    // { age: 25, name: 'Bar', isDev: true }
+    // { age: 29, name: 'Beer', isDev: false }
+  }
+
+  tapOperator() {
+    console.log('tapOperator');
+
+    of(1, 2, 3, 4, 5)
+      .pipe(
+        tap(val => {
+          console.log("before " + val);
+        }),
+        map(val => {
+          return val + 5;
+        }),
+        tap(val => {
+          console.log("after " + val);
+        })
+      )
+      .subscribe(val => console.log(val));
+
+
+    let list1 = of(1, 2, 3, 4, 5, 6);
+    let final_val = list1.pipe(
+      tap(x => console.log("From tap() =" + x),
+        e => console.log(e),
+        () => console.log("Task complete")),
+      filter(a => a % 2 === 0)
+    );
+    final_val.subscribe(x => console.log("Only Even numbers=" + x));
+
+  }
+
+
+  conCatMapOperator() {
+    let srcObservable = of(1, 2, 3, 4)
+    let innerObservable = of('A', 'B', 'C', 'D')
+
+    srcObservable.pipe(
+      concatMap(val => {
+        console.log('Source value ' + val)
+        console.log('starting new observable')
+        return innerObservable
+      })
+    )
+      .subscribe(ret => {
+        console.log('Recd ' + ret);
+      })
+  }
+
+
+  exhaustMapOperator() {
+
+    let srcObservable = of(1, 2, 3, 4)
+    let innerObservable = of('A', 'B', 'C', 'D')
+
+    srcObservable.pipe(
+      exhaustMap(val => {
+        console.log('Source value ' + val)
+        console.log('starting new observable')
+        return innerObservable
+      })
+    )
+      .subscribe(ret => {
+        console.log('Recd ' + ret);
+      })
+  }
+
+  switchMapOperator() {
+    let text = of('Hello guys, Welcome To');
+    let case1 = text.pipe(switchMap((value) => of(value + ' JavaTpoint!')));
+    case1.subscribe((value) => { console.log(value); });
+  }
+
+  exhaustAllOperator() {
+    console.log('exhaustAllOperator');
+    
+    const clicks = fromEvent(document, 'click');
+    const higherOrder = clicks.pipe(
+      map(() => interval(1000).pipe(take(5)))
+    );
+    const result = higherOrder.pipe(exhaustAll());
+    result.subscribe(x => console.log(x));
   }
 
 
